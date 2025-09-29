c30dccab0fefc856c51eb9f1e6b85eec23b6b9e8
Znalreh <skulxhmp@gmail.com>
2025-09-29 21:39:37 +0800

    setup(teknoy_mart): create register-page

--- teknoy_mart/teknoy_mart/settings.py
+++ teknoy_mart/teknoy_mart/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'teknoy_mart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "static" / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +115,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# If you want to collect all static files in one place for deployment
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
