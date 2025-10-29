7785e07973dd56cf856b8f5912a600ad5c9e4fc6
Adrian O. Pateres <adpateres@gmail.com>
2025-10-30 02:18:21 +0800

    Allow host

--- teknoy_mart/teknoy_mart/settings.py
+++ teknoy_mart/teknoy_mart/settings.py
@@ -37,7 +37,14 @@ SECRET_KEY = 'django-insecure-*cbq@_@!0cqhg73jo+)^a7nqbucgu*35jbbjx&2d7z*^)7#b8-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
+
+CSRF_TRUSTED_ORIGINS = [# allow Render subdomains (Django requires scheme)
+    "https://*.onrender.com",
+]
 
 
 # Application definition
@@ -138,8 +145,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"] if (BASE_DIR / "static").exists() else []
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
