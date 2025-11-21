4155d3aef10fd1ff4fdb588afd2e84808c3709e7
Adrian O. Pateres <adpateres@gmail.com>
2025-11-21 19:36:20 +0800

    Added Cloudinary for permanent image storage

--- teknoy_mart/teknoy_mart/settings.py
+++ teknoy_mart/teknoy_mart/settings.py
@@ -57,6 +57,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'cloudinary_storage',
+    'cloudinary',
     'teknoymart',
 ]
 
